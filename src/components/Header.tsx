dd8ad3c5f9d8b02fcbc4ab69f7079fc3245814be
Patrik Kolenak <PATRIK@MacBook-Pro.local>
2026-04-02 04:00:39 +0200

    fix: logo-groow.png is SVG, rename to .svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="bg-[#3d0088] pt-12 pb-8 border-b border-[#75ffe5]">
       <div className="max-w-[1140px] mx-auto px-4 flex justify-center animate-fadeIn">
         <img
-          src="/images/logo-groow.png"
+          src="/images/logo-groow.svg"
           alt="GROOW"
           className="max-w-[180px] w-full"
         />
